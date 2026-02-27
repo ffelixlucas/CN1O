2e0ec27237d19bda6ed0266201998515489d0bac
Lucas Felix <lucas.fafx@gmail.com>
2026-02-27 15:17:21 -0300

    refactor: manter apenas header no HomeLayout para etapa inicial

--- src/layouts/HomeLayout.ts
+++ src/layouts/HomeLayout.ts
@@ -1,17 +1,13 @@
 import { Header } from '../components/Header';
-import { AboutSection } from '../sections/AboutSection';
-import { ClassesSection } from '../sections/ClassesSection';
-import { EventsSection } from '../sections/EventsSection';
-import { LocationSection } from '../sections/LocationSection';
-import { Footer } from '../components/Footer';
+//import { AboutSection } from '../sections/AboutSection';
+//import { ClassesSection } from '../sections/ClassesSection';
+//import { EventsSection } from '../sections/EventsSection';
+//import { LocationSection } from '../sections/LocationSection';
+//import { Footer } from '../components/Footer';
 
 export function HomeLayout() {
   return `
     ${Header()}
-    ${AboutSection()}
-    ${ClassesSection()}
-    ${EventsSection()}
-    ${LocationSection()}
-    ${Footer()}
+
   `;
 }
